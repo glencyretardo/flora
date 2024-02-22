0aeb995694911e6c4a79506b13fa3496ea309530
glencyretardo <g.retardo.534306@umindanao.edu.ph>
2024-02-22 17:08:47 +0800

    first commit

---
+++ home.php
@@ -0,0 +1,71 @@
+<?php
+
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php include 'header.php'; ?>
+
+    <section class="home">
+
+        <div class="content">
+            <h3>new collections</h3>
+            <p>Unleash the magic of nature with flora. 
+                From minimalist elegance to bursts of vibrant color. </p>
+            <a href="about.php" class="btn">discover more</a>
+        </div>
+
+    </section>
+
+    <section class="products">
+
+        <h1 class="title">best sellers</h1>
+
+        <div class="box-container">
+
+           
+                    <form action="" method="POST" class="box">
+            
+                        <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+                        <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+                    </form>
+           
+
+        </div>
+
+        <div class="more-btn">
+            <a href="shop.php" class="option-btn">load more</a>
+        </div>
+
+    </section>
+
+    <section class="home-contact">
+
+        <div class="content">
+            <h3>have any questions?</h3>
+            <a href="contact.php" class="btn">contact us</a>
+        </div>
+
+    </section>
+
+    <?php include 'footer.php'; ?>
+
+
+</body>
+
+</html>
---
+++ view_page.php
@@ -0,0 +1,71 @@
+<?php
+
+
+if(isset($_POST['add_to_wishlist'])){
+
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    
+
+}
+
+if(isset($_POST['add_to_cart'])){
+
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>quick view</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+   <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="quick-view">
+
+    <h1 class="title">product details</h1>
+
+    <form action="" method="POST">
+         <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
+         <div class="name"><?php echo $fetch_products['name']; ?></div>
+         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+         <div class="details"><?php echo $fetch_products['details']; ?></div>
+         <input type="number" name="product_quantity" value="1" min="0" class="qty">
+         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
+         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+         <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+      </form>
+
+    <div class="more-btn">
+        <a href="home.php" class="option-btn">go to home page</a>
+    </div>
+
+</section>
+
+
+
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
