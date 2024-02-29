0f5cec5aad4eba0a5112d39af2a3b2be0a08076d
juliaverzosa <j.verzosa.534381@umindanao.edu.ph>
2024-03-01 02:05:28 +0800

    admin side update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -34,33 +34,66 @@ session_start();
       <div class="box">
          
          <!-- gawas diri tung mga pending money na orders na wa pa nabayaran -->
-            
+         <?php
+            $total_pendings = 0;
+            $select_pendings = mysqli_query($conn, "SELECT * FROM `orders` WHERE payment_status = 'pending'") or die('query failed');
+            while($fetch_pendings = mysqli_fetch_assoc($select_pendings)){
+               $total_pendings += $fetch_pendings['total_price'];
+            };
+         ?>
+         <h3>$<?php echo $total_pendings; ?>/-</h3>
          <p>total pendings</p>
       </div>
 
       <div class="box">
         <!-- gawas diri kita -->
+        <?php
+            $total_completes = 0;
+            $select_completes = mysqli_query($conn, "SELECT * FROM `orders` WHERE payment_status = 'completed'") or die('query failed');
+            while($fetch_completes = mysqli_fetch_assoc($select_completes)){
+               $total_completes += $fetch_completes['total_price'];
+            };
+         ?>
          <p>completed payments</p>
       </div>
 
       <div class="box">
       <!-- gawas diri pila nay na placed order -->
+      <?php
+            $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
+            $number_of_orders = mysqli_num_rows($select_orders);
+         ?>
+         <h3><?php echo $number_of_orders; ?></h3>
          <p>orders placed</p>
       </div>
 
       <div class="box">
         <!-- gawas diri dapat pila ka products naa add -->
+        <?php
+            $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+            $number_of_products = mysqli_num_rows($select_products);
+         ?>
          <p>products added</p>
       </div>
 
       <div class="box">
+      <?php
+            $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'user'") or die('query failed');
+            $number_of_users = mysqli_num_rows($select_users);
+         ?>
+         <h3><?php echo $number_of_users; ?></h3>
          <!-- gawas pila ka users naa -->
          <p>users</p>
       </div>
 
 
       <div class="box">
-         <!-- gawas diri pila ka messages nadawat gikan didto sa contact -->
+         <!-- gawas diri pila ka messages nadawat gikan didto sa contact-->
+         <?php
+            $select_messages = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
+            $number_of_messages = mysqli_num_rows($select_messages);
+         ?>
+         <h3><?php echo $number_of_messages; ?></h3>
          <p>messages</p>
       </div>
 
