284e2fc95a5abaa878e1e0cb501316c5a7f87d3b
glencyretardo <g.retardo.534306@umindanao.edu.ph>
2024-03-01 22:50:08 +0800

    try langs sa shop

--- shop.php
+++ shop.php
@@ -1,6 +1,6 @@
 <?php
 
-
+require_once 'database.php';
 session_start();
 
 
@@ -26,6 +26,8 @@ if(isset($_POST['add_to_cart'])){
 
 
 }
+
+$select_products = mysqli_query($conn, "SELECT * FROM `product`") or die('query failed');
 
 ?>
 
@@ -55,47 +57,25 @@ if(isset($_POST['add_to_cart'])){
 
 <section class="products">
 
-   <h1 class="title">sale</h1>
-
-   <div class="box-container">
-      <form action="" method="POST" class="box">
-         <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
-         <!-- <div class="price">$<?php echo $fetch_products['price']; ?>/-</div> -->
-         <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
-         <!-- <div class="name"><?php echo $fetch_products['name']; ?></div> -->
-         <input type="number" name="product_quantity" value="1" min="0" class="qty">
-         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
-         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-         <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
-         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-      </form>
-   </div>
-
-   
-   <h1 class="title">best sellers</h1>
-
-   <div class="box-container">
-      <form action="" method="POST" class="box">
-         <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
-         <!-- <div class="price">$<?php echo $fetch_products['price']; ?>/-</div> -->
-         <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
-         <!-- <div class="name"><?php echo $fetch_products['name']; ?></div> -->
-         <input type="number" name="product_quantity" value="1" min="0" class="qty">
-         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
-         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-         <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
-         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-      </form>
-   </div>
+   <?php while ($fetch_products = mysqli_fetch_assoc($select_products)) { ?>
+      <div class="box-container">
+         <form action="" method="POST" class="box">
+            <a href="view_page.php?pid=<?php echo $fetch_products['ProductID']; ?>" class="fas fa-eye"></a>
+            <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
+            <div class="name"><?php echo $fetch_products['ProductName']; ?></div>
+            <div class="price">$<?php echo $fetch_products['Price']; ?>/-</div>
+            <input type="number" name="product_quantity" value="1" min="0" class="qty">
+            <input type="hidden" name="product_id" value="<?php echo $fetch_products['ProductID']; ?>">
+            <input type="hidden" name="product_name" value="<?php echo $fetch_products['ProductName']; ?>">
+            <input type="hidden" name="product_price" value="<?php echo $fetch_products['Price']; ?>">
+            <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+            <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+            <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+         </form>
+      </div>
+   <?php } ?>
 
 </section>
-
-
-
 
 <?php include 'footer.php'; ?>
 
