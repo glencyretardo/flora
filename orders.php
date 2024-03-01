959654cea884fe310ffa4435af76112f63bb72e5
glencyretardo <g.retardo.534306@umindanao.edu.ph>
2024-03-02 01:04:13 +0800

    edit orders

--- orders.php
+++ orders.php
@@ -1,9 +1,11 @@
 <?php
 
-
+require_once  'database.php';
 session_start();
 
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -25,28 +27,37 @@ session_start();
     <p> <a href="home.php">home</a> / orders </p>
 </section>
 
+
+
 <section class="placed-orders">
 
     <h1 class="title">placed orders</h1>
 
     <div class="box-container">
-
- 
-    <div class="box">
-        <p> placed on : <span><?php echo $fetch_orders['OrderDate']; ?></span> </p>
-        <p> name : <span><?php echo $fetch_orders['Name']; ?></span> </p>
-        <p> number : <span><?php echo $fetch_orders['number']; ?></span> </p>
-        <p> email : <span><?php echo $fetch_orders['Email']; ?></span> </p>
-        <p> address : <span><?php echo $fetch_orders['Address']; ?></span> </p>
-        <p> payment method : <span><?php echo $fetch_orders['PaymentMethod']; ?></span> </p>
-        <p> your orders : <span><?php echo $fetch_orders['TotalProducts']; ?></span> </p>
-        <p> total price : <span>$<?php echo $fetch_orders['TotalAmount']; ?>/-</span> </p>
-        <p> payment status : <span style="color:<?php if($fetch_orders['payment_status'] == 'pending'){echo 'tomato'; }else{echo 'green';} ?>"><?php echo $fetch_orders['payment_status']; ?></span> </p>
+        <?php
+        $select_orders = mysqli_query($conn, "SELECT * FROM `order`") or die('query failed');
+        if (mysqli_num_rows($select_orders) > 0) {
+            while ($fetch_orders = mysqli_fetch_assoc($select_orders)) {
+                ?>
+                <div class="box">
+                    <p> placed on : <span><?php echo $fetch_orders['OrderDate']; ?></span> </p>
+                    <p> name : <span><?php echo $fetch_orders['Name']; ?></span> </p>
+                    <p> number : <span><?php echo $fetch_orders['number']; ?></span> </p>
+                    <p> email : <span><?php echo $fetch_orders['Email']; ?></span> </p>
+                    <p> address : <span><?php echo $fetch_orders['Address']; ?></span> </p>
+                    <p> payment method : <span><?php echo $fetch_orders['PaymentMethod']; ?></span> </p>
+                    <p> your orders : <span><?php echo $fetch_orders['TotalProducts']; ?></span> </p>
+                    <p> total price : <span>$<?php echo $fetch_orders['TotalAmount']; ?>/-</span> </p>
+                    <p> payment status : <span style="color:<?php echo ($fetch_orders['payment_status'] == 'pending') ? 'tomato' : 'green'; ?>"><?php echo $fetch_orders['payment_status']; ?></span> </p>
+                </div>
+            <?php
+            }
+        } else {
+            echo "No orders found.";
+        }
+        ?>
     </div>
-    </div>
-
 </section>
-
 
 <?php include 'footer.php'; ?>
 
