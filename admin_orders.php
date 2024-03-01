5d4c9b0d3a94436f4cd010683e21559964437d27
juliaverzosa <j.verzosa.534381@umindanao.edu.ph>
2024-03-01 15:56:37 +0800

    admin orders with style

--- admin_orders.php
+++ admin_orders.php
@@ -24,7 +24,7 @@ if(isset($_GET['delete'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>dashboard</title>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-   <link rel="stylesheet" href="css/admin_style.css">
+   <link rel="stylesheet" href="admin_style.css">
 
 </head>
 <body>
