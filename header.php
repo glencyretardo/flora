c04a9501909cefed220cf135130da6a5c3c22433
glencyretardo <g.retardo.534306@umindanao.edu.ph>
2024-03-02 14:03:18 +0800

    wa pa nafix ang redirect sa home

--- header.php
+++ header.php
@@ -1,11 +1,14 @@
 <?php
+
+
 function isUserLoggedIn() {
     return isset($_SESSION['user_id']);
 }
+
 function logout() {
     session_unset();
     session_destroy();
-    header('location: login.php');
+    header('Location: login.php');
     exit();
 }
 ?>
--- logout.php
+++ logout.php
@@ -1,9 +1,14 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 session_unset();
 session_destroy();
 
+//echo 'Logout successful';
 header('location:home.php');
-exit();
+//exit();
 ?>
