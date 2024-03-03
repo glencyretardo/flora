838424763308825a78ccc8b174afe6ce5f70ea00
juliaverzosa <j.verzosa.534381@umindanao.edu.ph>
2024-03-03 18:20:48 +0800

    changes home page

--- wishlist.php
+++ wishlist.php
@@ -98,7 +98,7 @@ if (isset($_GET['delete_all'])) {
                     <a href="view_page.php?pid=<?php echo $fetch_wishlist['ProductID']; ?>" class="fas fa-eye"></a>
                     <img src="uploaded_img/<?php echo $fetch_wishlist['image']; ?>" alt="" class="image">
                     <div class="name"><?php echo $fetch_wishlist['product_name']; ?></div>
-                    <div class="price">$<?php echo $fetch_wishlist['price']; ?>/-</div>
+                    <div class="price">₱<?php echo $fetch_wishlist['price']; ?></div>
                     <input type="hidden" name="product_id" value="<?php echo $fetch_wishlist['ProductID']; ?>">
                     <input type="hidden" name="product_name" value="<?php echo $fetch_wishlist['product_name']; ?>">
                     <input type="hidden" name="product_price" value="<?php echo $fetch_wishlist['price']; ?>">
