c72769b9ca9885731225286a6b9d6b3e665e8659
glencyretardo <g.retardo.534306@umindanao.edu.ph>
2024-03-03 22:00:02 +0800

    gi update ang update products

--- admin_update_product.php
+++ admin_update_product.php
@@ -27,7 +27,7 @@ if(isset($_POST['update_product'])){
          mysqli_query($conn, "UPDATE `product` SET image = '$image' WHERE ProductID = '$update_p_id'") or die('query failed');
          move_uploaded_file($image_tmp_name, $image_folter);
          unlink('uploaded_img/'.$old_image);
-         $message[] = 'image updated successfully!';
+       
       }
    }
 
@@ -71,9 +71,9 @@ if(isset($_GET['update'])){
    <img src="uploaded_img/<?php echo $fetch_product['image']; ?>" class="image"  alt="">
    <input type="hidden" value="<?php echo $fetch_product['ProductID']; ?>" name="update_p_id">
    <input type="hidden" value="<?php echo $fetch_product['image']; ?>" name="update_p_image">
-   <input type="text" class="box" value="<?php echo $fetch_product['ProductName']; ?>" required placeholder="update product name" name="name">
-   <input type="number" min="0" class="box" value="<?php echo $fetch_product['Price']; ?>" required placeholder="update product price" name="price">
-   <textarea name="details" class="box" required placeholder="update product details" cols="30" rows="10"><?php echo $fetch_product['details']; ?></textarea>
+   <input type="text" class="box" value="<?php echo $fetch_product['ProductName']; ?>" required placeholder="update product name" name="ProductName">
+   <input type="number" min="0" class="box" value="<?php echo $fetch_product['Price']; ?>" required placeholder="update product price" name="Price">
+   <textarea name="details" class="box" required placeholder="update product details" cols="30" rows="10"><?php echo $fetch_product['Description']; ?></textarea>
    <input type="file" accept="image/jpg, image/jpeg, image/png" class="box" name="image">
    <input type="submit" value="update product" name="update_product" class="btn">
    <a href="admin_products.php" class="option-btn">go back</a>
